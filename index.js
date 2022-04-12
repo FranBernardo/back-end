46a9445a8efdea809214fece95ce7dc9cfbea74b
fran <francielebernardo88@hotmail.com>
2022-04-12 15:17:52 -0300

    separando o servidor

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ mongoose.connect(url,
   })
 .then(() => {
     console.log('connectamos ao mongoDB')
-    app.listen(process.env.PORT || 8080)
 })
 .catch((err) => console.log(err))
+app.listen(process.env.PORT || 8080, () => `servidor iniciado`)
